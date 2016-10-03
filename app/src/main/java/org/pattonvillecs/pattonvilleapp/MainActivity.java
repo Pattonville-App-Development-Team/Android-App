02f372880d5ad960cd92cdfad816c10bed185799
Nathaniel Skelton <nathan.hunter.skelton@gmail.com>
2016-10-03 09:34:11 -0500

    Fixed the loading of the first fragment on the opening of the app

--- app/src/main/java/org/pattonvillecs/pattonvilleapp/MainActivity.java
+++ app/src/main/java/org/pattonvillecs/pattonvilleapp/MainActivity.java
@@ -34,6 +34,9 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        navigationView.getMenu().getItem(0).setChecked(true);
+        onNavigationItemSelected(navigationView.getMenu().getItem(0));
     }
 
     @Override
