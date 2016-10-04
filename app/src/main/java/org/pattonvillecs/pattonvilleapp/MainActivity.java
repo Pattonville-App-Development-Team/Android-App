c8d04ae2f9de218b12b6a44cf08443a0e9aa1cc0
Nathaniel Skelton <nathan.hunter.skelton@gmail.com>
2016-10-04 10:01:15 -0500

    Added Settings Activity and Fragment setup

--- app/src/main/java/org/pattonvillecs/pattonvilleapp/MainActivity.java
+++ app/src/main/java/org/pattonvillecs/pattonvilleapp/MainActivity.java
@@ -2,6 +2,7 @@ package org.pattonvillecs.pattonvilleapp;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -10,6 +11,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -83,7 +85,7 @@ public class MainActivity extends AppCompatActivity
         switch (item.getItemId()) {
 
             case R.id.nav_home:
-
+                Log.e("Main Activity", "cased into nav_home");
                 fragment = HomeFragment.newInstance();
                 break;
             case R.id.nav_news:
@@ -103,6 +105,9 @@ public class MainActivity extends AppCompatActivity
             case R.id.nav_peachjar:
 
                 break;
+            case R.id.nav_settings:
+                Log.e("Main Activity", "cased into action_settings");
+                startActivity(new Intent(this, SettingsActivity.class));
         }
 
         if (fragment != null) {
---
+++ app/src/main/java/org/pattonvillecs/pattonvilleapp/SettingsActivity.java
@@ -0,0 +1,22 @@
+package org.pattonvillecs.pattonvilleapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.pattonvillecs.pattonvilleapp.fragments.SettingsFragment;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Display the fragment as the main content.
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
+}
