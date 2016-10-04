c5a26e8f1bf0b0cf391126d9464b2bbf06a17572
Mitchell Skaggs <skaggsm333@gmail.com>
2016-10-04 12:20:58 -0500

    Converted SettingsFragment to support v7 libraries

--- app/src/main/java/org/pattonvillecs/pattonvilleapp/fragments/SettingsFragment.java
+++ app/src/main/java/org/pattonvillecs/pattonvilleapp/fragments/SettingsFragment.java
@@ -1,7 +1,7 @@
 package org.pattonvillecs.pattonvilleapp.fragments;
 
 import android.os.Bundle;
-import android.preference.PreferenceFragment;
+import android.support.v7.preference.PreferenceFragmentCompat;
 
 import org.pattonvillecs.pattonvilleapp.R;
 
@@ -9,12 +9,30 @@ import org.pattonvillecs.pattonvilleapp.R;
  * Created by skeltonn on 10/4/16.
  */
 
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    public SettingsFragment() {
+        super();
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment SettingsFragment.
+     */
+    public static SettingsFragment newInstance() {
+        SettingsFragment fragment = new SettingsFragment();
+        return fragment;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
 
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
     }
