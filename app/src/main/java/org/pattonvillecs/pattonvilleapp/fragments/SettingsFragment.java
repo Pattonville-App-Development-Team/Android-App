6519ee497494871de015d1c0dca532b7a53855a7
Nathaniel Skelton <nathan.hunter.skelton@gmail.com>
2016-10-04 13:12:55 -0500

    Structured settings menu

--- app/src/main/java/org/pattonvillecs/pattonvilleapp/fragments/SettingsFragment.java
+++ app/src/main/java/org/pattonvillecs/pattonvilleapp/fragments/SettingsFragment.java
@@ -2,12 +2,12 @@ package org.pattonvillecs.pattonvilleapp.fragments;
 
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
 
 import org.pattonvillecs.pattonvilleapp.R;
-
-/**
- * Created by skeltonn on 10/4/16.
- */
 
 public class SettingsFragment extends PreferenceFragment {
 
@@ -17,5 +17,16 @@ public class SettingsFragment extends PreferenceFragment {
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.preferences);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = super.onCreateView(inflater, container, savedInstanceState);
+        if (v != null) {
+            ListView lv = (ListView) v.findViewById(android.R.id.list);
+            lv.setPadding(16, 16, 16, 16);
+        }
+        return v;
     }
 }
