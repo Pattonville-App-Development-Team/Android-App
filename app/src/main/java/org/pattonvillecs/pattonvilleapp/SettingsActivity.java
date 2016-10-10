c879b80185f372e6d6fff8baed6c30ac306ce0bc
Nathaniel Skelton <nathan.hunter.skelton@gmail.com>
2016-10-09 19:00:22 -0500

    Resolved issue with back/up navigation from settings activity

--- app/src/main/java/org/pattonvillecs/pattonvilleapp/SettingsActivity.java
+++ app/src/main/java/org/pattonvillecs/pattonvilleapp/SettingsActivity.java
@@ -12,8 +12,6 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
         // Display the fragment as the main content.
         getSupportFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
