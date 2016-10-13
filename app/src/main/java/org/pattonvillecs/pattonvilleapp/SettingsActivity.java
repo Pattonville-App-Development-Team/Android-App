0d0bd8ae8b1d30e1431149c83d11953f39e5c257
Nathaniel Skelton <nathan.hunter.skelton@gmail.com>
2016-10-13 12:17:43 -0500

    Made SettingsFragment a subclass of S.Activity, applied theme and corrected colors

--- app/src/main/java/org/pattonvillecs/pattonvilleapp/SettingsActivity.java
+++ app/src/main/java/org/pattonvillecs/pattonvilleapp/SettingsActivity.java
@@ -3,7 +3,7 @@ package org.pattonvillecs.pattonvilleapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import org.pattonvillecs.pattonvilleapp.fragments.SettingsFragment;
+import com.takisoft.fix.support.v7.preference.PreferenceFragmentCompat;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -16,5 +16,20 @@ public class SettingsActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            getActivity().setTheme(R.style.PSD_Preference);
+            super.onCreate(savedInstanceState);
+        }
+
+        @Override
+        public void onCreatePreferencesFix(Bundle savedInstanceState, String rootKey) {
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.preferences);
+        }
     }
 }
