6d2b2897d6a01f716dfe186da34ab57b05a8beb3
Mitchell Skaggs <skaggsm333@gmail.com>
2016-10-22 11:11:52 -0500

    Added potential undo button to add to calendar feature

--- app/src/main/java/org/pattonvillecs/pattonvilleapp/fragments/calendar/CalendarEventDetailsActivity.java
+++ app/src/main/java/org/pattonvillecs/pattonvilleapp/fragments/calendar/CalendarEventDetailsActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 import org.pattonvillecs.pattonvilleapp.R;
 
@@ -17,15 +18,25 @@ public class CalendarEventDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_calendar_event_details);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        //noinspection ConstantConditions
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar snackbar = Snackbar.make(view, "Added to your calendar", Snackbar.LENGTH_LONG);
+                /*if (Build.VERSION.SDK_INT >= 23)
+                    snackbar = snackbar.setActionTextColor(getResources().getColor(R.color.colorAccent, getTheme()));
+                else
+                    snackbar = snackbar.setActionTextColor(getResources().getColor(R.color.colorAccent));*/
+                snackbar.setAction("Undo", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(CalendarEventDetailsActivity.this, "Sorry, undo is not supported at this time.", Toast.LENGTH_SHORT).show();
+                    }
+                }).show();
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
