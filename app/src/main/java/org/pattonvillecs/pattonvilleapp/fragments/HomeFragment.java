2419d8f9428406d7988989934505460ebde76f80
Ethan Holtgrieve <erholtgrieve@gmail.com>
2016-11-16 09:47:20 -0600

    Added news items on home fragment

--- app/src/main/java/org/pattonvillecs/pattonvilleapp/fragments/HomeFragment.java
+++ app/src/main/java/org/pattonvillecs/pattonvilleapp/fragments/HomeFragment.java
@@ -9,17 +9,29 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageListener;
 
 import org.pattonvillecs.pattonvilleapp.R;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class HomeFragment extends Fragment {
 
     CarouselView carouselView;
 
+    ListView newsListView;
+
+    String[] sampleHeadlines = {"Student named _____ of the Year", "Pattonville Robotics Club wins Super Internationals", "Third Headline"};
+
     int[] sampleImages = {R.drawable.test_news_1, R.drawable.test_news_2, R.drawable.test_news_3, R.drawable.test_news_4};
+
+
     ImageListener imageListener = new ImageListener() {
         @Override
         public void setImageForPosition(int position, ImageView imageView) {
@@ -55,7 +67,31 @@ public class HomeFragment extends Fragment {
         carouselView = (CarouselView) view.findViewById(R.id.carouselView);
         carouselView.setPageCount(4);
         carouselView.setImageListener(imageListener);
+
+
+        newsListView = (ListView) view.findViewById(R.id.home_news_listview);
+
+        List<HashMap<String, String>> homeNewsList = new ArrayList<HashMap<String, String>>();
+
+        for (int i = 0; i < 3; i++) {
+
+            HashMap<String, String> newsListItem = new HashMap<String, String>();
+            newsListItem.put("image", Integer.toString(sampleImages[i]));
+            newsListItem.put("headline", sampleHeadlines[i]);
+            homeNewsList.add(newsListItem);
+        }
+
+        String[] from = {"image", "headline"};
+
+        int[] to = {R.id.home_news_listview_item_imageView, R.id.home_news_listview_item_textView};
+
+        SimpleAdapter adapter = new SimpleAdapter(view.getContext(), homeNewsList, R.layout.home_news_listview_item, from, to);
+
+        newsListView.setAdapter(adapter);
+
         return view;
+
+
     }
 
     public void onButtonPressed(Uri uri) {
