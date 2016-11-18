e0315aa9c2be447a9457f16e8468a010a612a054
Nathaniel Skelton <nathan.hunter.skelton@gmail.com>
2016-11-18 14:13:22 -0600

    Added Firebase to the project, working notifications when app closed

---
+++ app/src/main/java/org/pattonvillecs/pattonvilleapp/FBNotificationsService.java
@@ -0,0 +1,33 @@
+package org.pattonvillecs.pattonvilleapp;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class FBNotificationsService extends FirebaseMessagingService {
+
+    public final String TAG = "NotificationsService";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // ...
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.e(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.e(TAG, "Message data payload: " + remoteMessage.getData());
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.e(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+    }
+}
