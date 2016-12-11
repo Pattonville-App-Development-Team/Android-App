692c9ce78f0e7ff36962f32cebd279fadc3d2167
Mitchell Skaggs <skaggsm333@gmail.com>
2016-12-11 11:33:25 -0600

    Fully documented CalendarEvent class

--- app/src/main/java/org/pattonvillecs/pattonvilleapp/fragments/calendar/CalendarEvent.java
+++ app/src/main/java/org/pattonvillecs/pattonvilleapp/fragments/calendar/CalendarEvent.java
@@ -2,12 +2,15 @@ package org.pattonvillecs.pattonvilleapp.fragments.calendar;
 
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.support.annotation.NonNull;
 
 import com.annimon.stream.Optional;
 import com.annimon.stream.function.Function;
+import com.annimon.stream.function.Supplier;
 
 import net.fortuna.ical4j.model.component.VEvent;
 import net.fortuna.ical4j.model.property.Description;
+import net.fortuna.ical4j.model.property.DtStart;
 import net.fortuna.ical4j.model.property.Location;
 import net.fortuna.ical4j.model.property.Summary;
 
@@ -15,9 +18,18 @@ import java.util.Date;
 
 /**
  * Created by Mitchell on 10/18/2016.
+ * <p>
+ * This class represents a single calendar event.
+ *
+ * @author Mitchell Skaggs
+ * @see android.os.Parcelable
+ * @since 1.0
  */
 
 public class CalendarEvent implements Parcelable {
+    /**
+     * Helper for parcelable serialization
+     */
     public static final Creator<CalendarEvent> CREATOR = new Creator<CalendarEvent>() {
         @Override
         public CalendarEvent createFromParcel(Parcel in) {
@@ -29,33 +41,79 @@ public class CalendarEvent implements Parcelable {
             return new CalendarEvent[size];
         }
     };
+    /**
+     * The name of the event. Equal to the empty string if not provided.
+     */
+    @NonNull
     private final String eventName;
+    /**
+     * The date that the event starts on. Always present.
+     */
+    @NonNull
     private final Date dateAndTime;
+    /**
+     * The details of the event. Equal to the empty string if not provided.
+     */
+    @NonNull
     private final String eventDetails;
+    /**
+     * The name of the location of the event. Equal to the empty string if not provided.
+     */
+    @NonNull
     private final String eventLocation;
 
-    public CalendarEvent(String eventName, Date dateAndTime, String eventDetails, String eventLocation) {
+    /**
+     * A constructor that takes every part individually. Typically called by other constructors.
+     *
+     * @param eventName     The name of the event. May be the empty string.
+     * @param dateAndTime   The date of the event. Must be valid.
+     * @param eventDetails  The details of the event. May be the empty string.
+     * @param eventLocation The name of the location of the event. May be the empty string.
+     */
+    public CalendarEvent(@NonNull String eventName, @NonNull Date dateAndTime, @NonNull String eventDetails, @NonNull String eventLocation) {
         this.eventName = eventName;
         this.dateAndTime = dateAndTime;
         this.eventDetails = eventDetails;
         this.eventLocation = eventLocation;
     }
 
-    protected CalendarEvent(Parcel in) {
+    /**
+     * A constructor that takes a parcel containing the calendar event.
+     * <p>
+     * Extracts in this order: name, date, details, location.
+     *
+     * @param in The parcel from which the new calendar event is extracted
+     */
+    protected CalendarEvent(@NonNull Parcel in) {
         eventName = in.readString();
         dateAndTime = (Date) in.readSerializable();
         eventDetails = in.readString();
         eventLocation = in.readString();
     }
 
-    public CalendarEvent(VEvent calendarVEvent) {
-        this(Optional.ofNullable(calendarVEvent.getSummary()).map(new Function<Summary, String>() {
+    /**
+     * A constructor that takes in a VEvent parsed from an iCal file using iCal4j.
+     *
+     * @param calendarVEvent The VEvent from which the new calendar event is created
+     */
+    public CalendarEvent(@NonNull VEvent calendarVEvent) {
+        this(Optional.ofNullable(calendarVEvent.getSummary()).map(new Function<Summary, String>() { //TODO Should the name be required? Is it ever not provided?
                     @Override
                     public String apply(Summary summary) {
                         return summary.getValue();
                     }
                 }).orElse(""),
-                calendarVEvent.getStartDate().getDate(),
+                Optional.ofNullable(calendarVEvent.getStartDate()).map(new Function<DtStart, Date>() {
+                    @Override
+                    public Date apply(DtStart dtStart) {
+                        return dtStart.getDate();
+                    }
+                }).orElseThrow(new Supplier<RuntimeException>() {
+                    @Override
+                    public RuntimeException get() {
+                        return new IllegalArgumentException("Date required!");
+                    }
+                }),
                 Optional.ofNullable(calendarVEvent.getDescription()).map(new Function<Description, String>() {
                     @Override
                     public String apply(Description description) {
@@ -70,18 +128,42 @@ public class CalendarEvent implements Parcelable {
                 }).orElse(""));
     }
 
+    /**
+     * Gets the name of the event.
+     *
+     * @return The name of the event. May be the empty string.
+     */
+    @NonNull
     public String getEventName() {
         return eventName;
     }
 
+    /**
+     * Gets the date of the event.
+     *
+     * @return The date and time fo the event.
+     */
+    @NonNull
     public Date getDateAndTime() {
         return dateAndTime;
     }
 
+    /**
+     * Gets the details of the event.
+     *
+     * @return The details of the event. May be the empty string.
+     */
+    @NonNull
     public String getEventDetails() {
         return eventDetails;
     }
 
+    /**
+     * Gets the name of the location of the event.
+     *
+     * @return The name of the location of the event. May be the empty string.
+     */
+    @NonNull
     public String getEventLocation() {
         return eventLocation;
     }
@@ -91,6 +173,11 @@ public class CalendarEvent implements Parcelable {
         return 0;
     }
 
+    /**
+     * Writes in this order: name, date, details, location.
+     * <p>
+     * {@inheritDoc}
+     */
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeString(eventName);
