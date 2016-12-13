d4c53c324487709d47704b5acf805e8093e6b98d
Mitchell Skaggs <skaggsm333@gmail.com>
2016-12-13 15:58:42 -0600

    Removed special API 22 case. Resolves #11

--- app/src/main/java/org/pattonvillecs/pattonvilleapp/fragments/calendar/CalendarDecoratorUtil.java
+++ app/src/main/java/org/pattonvillecs/pattonvilleapp/fragments/calendar/CalendarDecoratorUtil.java
@@ -44,16 +44,16 @@ public class CalendarDecoratorUtil {
         ColorStateList list = ColorStateList.valueOf(color);
         Drawable mask = generateCircleDrawable(Color.WHITE);
         RippleDrawable rippleDrawable = new RippleDrawable(list, null, mask);
-//        API 21
+        //API 21
         if (Build.VERSION.SDK_INT == Build.VERSION_CODES.LOLLIPOP) {
             rippleDrawable.setBounds(bounds);
         }
 
-//        API 22. Technically harmless to leave on for API 21 and 23, but not worth risking for 23+
-        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.LOLLIPOP_MR1) {
-            int center = (bounds.left + bounds.right) / 2;
-            rippleDrawable.setHotspotBounds(center, bounds.top, center, bounds.bottom);
-        }
+        //API 22. Technically harmless to leave on for API 21 and 23, but not worth risking for 23+
+        //if (Build.VERSION.SDK_INT == Build.VERSION_CODES.LOLLIPOP_MR1) {
+        //    int center = (bounds.left + bounds.right) / 2;
+        //    rippleDrawable.setHotspotBounds(center, bounds.top, center, bounds.bottom);
+        //}
 
         return rippleDrawable;
     }
