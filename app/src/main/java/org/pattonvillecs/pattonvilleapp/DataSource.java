d0a60a8de7583fda807ed05f9d8f5257fb78a042
Nathaniel Skelton <nathan.hunter.skelton@gmail.com>
2016-12-16 20:08:30 -0600

    Added short name to DataSource for easy school referencing, for example "Willow Brook Elementary School" is "Willow Brook"

--- app/src/main/java/org/pattonvillecs/pattonvilleapp/DataSource.java
+++ app/src/main/java/org/pattonvillecs/pattonvilleapp/DataSource.java
@@ -15,61 +15,61 @@ import java.util.Set;
  */
 
 public enum DataSource {
-    DISTRICT("Pattonville School District", 0, true, true, false, false, false, false,
+    DISTRICT("Pattonville School District", "District", 0, true, true, false, false, false, false,
             "11097 St. Charles Rock Road, St. Ann, MO 63074",
             "(314) 213-8500",
             null,
             null,
             Optional.<String>empty()),
-    HIGH_SCHOOL("Pattonville High School", 1, true, true, true, true, false, false,
+    HIGH_SCHOOL("Pattonville High School", "High School", 1, true, true, true, true, false, false,
             "2497 Creve Coeur Mill Road, Maryland Heights, MO 63043",
             "(314) 213-8500",
             "(314) 213-8351",
             "(314) 213-8696",
             Optional.of("https://www.peachjar.com/index.php?a=28&b=138&region=94969")),
-    HEIGHTS_MIDDLE_SCHOOL("Heights Middle School", 2, true, true, true, false, true, false,
+    HEIGHTS_MIDDLE_SCHOOL("Heights Middle School", "Heights", 2, true, true, true, false, true, false,
             "195 Fee Fee Road, Maryland Heights, MO 63043 ",
             "(314) 213-8500",
             "(314) 213-8333",
             "(314) 213-8633",
             Optional.<String>empty()),
-    HOLMAN_MIDDLE_SCHOOL("Holman Middle School", 3, true, true, true, false, true, false,
+    HOLMAN_MIDDLE_SCHOOL("Holman Middle School", "Holman", 3, true, true, true, false, true, false,
             "11055 St. Charles Rock Road, St. Ann, MO 63074",
             "(314) 213-8032",
             "(314) 213-8332",
             "(314) 213-8632",
             Optional.of("https://www.peachjar.com/index.php?a=28&b=138&region=94975")),
-    REMINGTON_TRADITIONAL_SCHOOL("Remington Traditional School", 4, true, true, true, false, true, true,
+    REMINGTON_TRADITIONAL_SCHOOL("Remington Traditional School", "Remingon", 4, true, true, true, false, true, true,
             "102 Fee Fee Rd, Maryland Heights, MO 63043",
             "(314) 213-8016",
             "(314) 213-8116",
             "(314) 213-8616",
             Optional.<String>empty()),
-    BRIDGEWAY_ELEMENTARY("Bridgeway Elementary School", 5, true, true, true, false, false, true,
+    BRIDGEWAY_ELEMENTARY("Bridgeway Elementary School", "Bridgeway", 5, true, true, true, false, false, true,
             "11635 Oakbury Court, Bridgeton, MO 63044",
             "(314) 213-8012",
             "(314) 213-8112",
             "(314) 213-8612",
             Optional.of("https://www.peachjar.com/index.php?a=28&b=138&region=94979")),
-    DRUMMOND_ELEMENTARY("Drummond Elementary School", 6, true, true, true, false, false, true,
+    DRUMMOND_ELEMENTARY("Drummond Elementary School", "Drumond", 6, true, true, true, false, false, true,
             "3721 St. Bridget Lane, St Ann, MO 63074",
             "(314) 213-8419",
             "(314) 213-8519",
             "(314) 213-8619",
             Optional.of("https://www.peachjar.com/index.php?a=28&b=138&region=94976")),
-    ROSE_ACRES_ELEMENTARY("Rose Acres Elementary School", 7, true, true, true, false, false, true,
+    ROSE_ACRES_ELEMENTARY("Rose Acres Elementary School", "Rose Acres", 7, true, true, true, false, false, true,
             "2905 Rose Acres Lane, Maryland Heights, MO 63043",
             "(314) 213-8017",
             "(314) 213-8117",
             "(314) 213-8617",
             Optional.of("https://www.peachjar.com/index.php?a=28&b=138&region=94970")),
-    PARKWOOD_ELEMENTARY("Parkwood Elementary School", 8, true, true, true, false, false, true,
+    PARKWOOD_ELEMENTARY("Parkwood Elementary School", "Parkwood", 8, true, true, true, false, false, true,
             "3199 Parkwood Lane, Maryland Heights, MO 63043",
             "(314) 213-8015",
             "(314) 213-8115",
             "(314) 213-8615",
             Optional.of("https://www.peachjar.com/index.php?a=28&b=138&region=94967")),
-    WILLOW_BROOK_ELEMENTARY("Willow Brook Elementary School", 9, true, true, true, false, false, true,
+    WILLOW_BROOK_ELEMENTARY("Willow Brook Elementary School", "Willow Brook", 9, true, true, true, false, false, true,
             "11022 Schuetz Road, Creve Coeur, MO 63146",
             "(314) 213-8018",
             "(314) 213-8118",
@@ -161,16 +161,17 @@ public enum DataSource {
             })));
 
 
-    public final String name, address, mainNumber, attendanceNumber, faxNumber;
+    public final String name, shortName, address, mainNumber, attendanceNumber, faxNumber;
     public final int id;
     public final boolean hasCalendar, hasNews, isDisableable, isHighSchool, isMiddleSchool, isElementarySchool;
     public final Optional<String> peachjarLink;
 
 
-    DataSource(String name, int id, boolean hasCalendar, boolean hasNews, boolean isDisableable,
+    DataSource(String name, String shortName, int id, boolean hasCalendar, boolean hasNews, boolean isDisableable,
                boolean isHighSchool, boolean isMiddleSchool, boolean isElementarySchool,
                String address, String mainNumber, String attendanceNumber, String faxNumber, Optional<String> peachjarLink) {
         this.name = name;
+        this.shortName = shortName;
         this.id = id;
         this.hasCalendar = hasCalendar;
         this.hasNews = hasNews;
