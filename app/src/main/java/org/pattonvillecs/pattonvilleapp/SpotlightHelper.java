dd23d625da4e472cc7c7fe9e04f25b66d2c66f02
Mitchell Skaggs <skaggsm333@gmail.com>
2016-12-24 09:25:34 -0600

    Registered a listener on each target to remove the corresponding Spotlight when that target is detached. Closes #14

--- app/src/main/java/org/pattonvillecs/pattonvilleapp/SpotlightHelper.java
+++ app/src/main/java/org/pattonvillecs/pattonvilleapp/SpotlightHelper.java
@@ -3,6 +3,7 @@ package org.pattonvillecs.pattonvilleapp;
 import android.app.Activity;
 import android.graphics.Color;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 
 import com.wooplr.spotlight.SpotlightView;
@@ -17,15 +18,20 @@ import java.util.LinkedList;
 public final class SpotlightHelper {
 
     private static final LinkedList<SpotlightView.Builder> builders = new LinkedList<>();
-    private static boolean spotlightCurrentlyOpen = false;
+    private static final String TAG = "SpotlightHelper";
+    private static volatile boolean spotlightCurrentlyOpen = false;
 
     private SpotlightHelper() {
     }
 
-    public static void showSpotlight(Activity activity, View target, String mainText, String subText) {
+    public static void showSpotlight(Activity activity, final View target, String mainText, String subText) {
+
+        // Get the colors to use. Pattonville green for highlights, white for text on dark background
         int primaryColor = ContextCompat.getColor(activity, R.color.colorPrimary);
         int accentColor = ContextCompat.getColor(activity, R.color.colorAccent);
-        builders.addLast(new SpotlightView.Builder(activity)
+
+        // Initialize the builder for the eventual Spotlight
+        final SpotlightView.Builder builder = new SpotlightView.Builder(activity)
                 .introAnimationDuration(400)
                 .enableRevealAnimation(true)
                 .performClick(false)
@@ -43,19 +49,70 @@ public final class SpotlightHelper {
                 .dismissOnTouch(true)
                 .dismissOnBackPress(true)
                 .enableDismissAfterShown(true)
-                .usageId("TEST" + Double.doubleToRawLongBits(Math.random())) //UNIQUE ID
-                .setListener(new SpotlightListener() {
-                    @Override
-                    public void onUserClicked(String usageID) {
-                        if (builders.size() > 0)
-                            builders.pop().show();
-                        else
-                            spotlightCurrentlyOpen = false;
-                    }
-                }));
+                .usageId("TEST" + Double.doubleToRawLongBits(Math.random())); //UNIQUE ID
+
+        // Create an OnAttachStateChangeListener that will remove the builder from the queue and reset the spotlightCurrentlyOpen flag if it doesn't find itself in the queue (Already been popped and shown)
+        final View.OnAttachStateChangeListener onAttachStateChangeListener = new View.OnAttachStateChangeListener() {
+
+            /**
+             * Only for logging
+             *
+             * @param v target view
+             */
+            @Override
+            public void onViewAttachedToWindow(View v) {
+                Log.e(TAG, "onAttach of " + builder);
+                Log.e(TAG, "BuilderQueue: " + builders);
+            }
+
+            /**
+             * Removes the corresponding builder. If not found, assumes that it has already been popped and is currently shown
+             *
+             * @param v target view
+             */
+            @Override
+            public void onViewDetachedFromWindow(View v) {
+                //Used to cut down on virtual method calls
+                LinkedList<SpotlightView.Builder> tempBuilders = builders;
+                Log.e(TAG, "onDetach of " + builder);
+                Log.e(TAG, "ToPop: " + tempBuilders);
+                //Try to remove it
+                boolean wasPresent = tempBuilders.remove(builder);
+                //Has this builder already been popped (shown)?
+                if (!wasPresent)
+                    spotlightCurrentlyOpen = false;
+                Log.e(TAG, "PostPop: " + tempBuilders);
+            }
+        };
+
+        // Create a listener for the eventual Spotlight that is notified when a Spotlight is closed normally. Removes the OnAttachStateChangeListener and shows the next builder if there is one
+        SpotlightListener spotlightListener = new SpotlightListener() {
+            /**
+             * Removes the target's OnAttachStateChangeListener and continues the chain of Spotlights. If there are no more, resets the spotlightCurrentlyOpen flag
+             *
+             * @param usageID ID of the Spotlight that was shut down
+             */
+            @Override
+            public void onUserClicked(String usageID) {
+                target.removeOnAttachStateChangeListener(onAttachStateChangeListener);
+                if (builders.size() > 0)
+                    builders.pop().show();
+                else
+                    spotlightCurrentlyOpen = false;
+            }
+        };
+        //Set the builder's listener. builder must be final, so it is mutated here, later, where spotlightListener is visible
+        builder.setListener(spotlightListener);
+
+        //Add the builder to the queue
+        builders.addLast(builder);
+        //Kick-start the queue if no spotlight is open
         if (builders.size() == 1 && !spotlightCurrentlyOpen) {
             builders.pop().show();
             spotlightCurrentlyOpen = true;
         }
+
+        //Set the Spotlight target's OnAttachStateChangeListener now, after everything has settled down and any Spotlights have started
+        target.addOnAttachStateChangeListener(onAttachStateChangeListener);
     }
 }
