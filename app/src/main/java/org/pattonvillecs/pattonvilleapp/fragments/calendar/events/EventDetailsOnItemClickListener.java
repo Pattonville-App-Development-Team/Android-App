69e414b442606b4cda2bd60e1099a5ff02094843
Mitchell Skaggs <skaggsm333@gmail.com>
2017-01-21 14:29:32 -0600

    Made view finding fall back to more reliable methods on failure

--- app/src/main/java/org/pattonvillecs/pattonvilleapp/fragments/calendar/events/EventDetailsOnItemClickListener.java
+++ app/src/main/java/org/pattonvillecs/pattonvilleapp/fragments/calendar/events/EventDetailsOnItemClickListener.java
@@ -13,7 +13,10 @@ import org.pattonvillecs.pattonvilleapp.DataSource;
 import org.pattonvillecs.pattonvilleapp.R;
 import org.pattonvillecs.pattonvilleapp.fragments.calendar.CalendarEventDetailsActivity;
 
+import java.util.Set;
+
 import eu.davidea.flexibleadapter.FlexibleAdapter;
+import eu.davidea.viewholders.FlexibleViewHolder;
 
 /**
  * Created by skaggsm on 1/9/17.
@@ -35,16 +38,28 @@ public class EventDetailsOnItemClickListener implements FlexibleAdapter.OnItemCl
         Intent intent = new Intent(activity, CalendarEventDetailsActivity.class).putExtra("calendarEvent", calendarEvent);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            View listItem = eventAdapter.getRecyclerView().getChildAt(position);
-            View schoolColorCircle = listItem.findViewById(R.id.school_color_circle);
-            View textTop = listItem.findViewById(R.id.text_top);
-            View textBottom = listItem.findViewById(R.id.text_bottom);
+            View listItem = eventAdapter.getRecyclerView().getLayoutManager().getChildAt(position); // Should work mostly
 
-            activity.startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(activity,
-                    Pair.create(textTop, activity.getResources().getString(R.string.text_top_transition_name)),
-                    Pair.create(textBottom, activity.getResources().getString(R.string.text_bottom_transition_name)),
-                    Pair.create(schoolColorCircle, activity.getResources().getString(R.string.school_color_circle_transition_name))
-            ).toBundle());
+            if (listItem == null) { // Resort to more drastic measures if the view isn't found easily
+                Set<FlexibleViewHolder> boundViewHolders = eventAdapter.getAllBoundViewHolders();
+                for (FlexibleViewHolder flexibleViewHolder : boundViewHolders)
+                    if (flexibleViewHolder.getAdapterPosition() == position) {
+                        listItem = flexibleViewHolder.itemView;
+                        break;
+                    }
+            }
+
+            if (listItem != null) {
+                View schoolColorCircle = listItem.findViewById(R.id.school_color_circle);
+                View textTop = listItem.findViewById(R.id.text_top);
+                View textBottom = listItem.findViewById(R.id.text_bottom);
+
+                activity.startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(activity,
+                        Pair.create(textTop, activity.getResources().getString(R.string.text_top_transition_name)),
+                        Pair.create(textBottom, activity.getResources().getString(R.string.text_bottom_transition_name)),
+                        Pair.create(schoolColorCircle, activity.getResources().getString(R.string.school_color_circle_transition_name))
+                ).toBundle());
+            }
         } else {
             activity.startActivity(intent);
         }
