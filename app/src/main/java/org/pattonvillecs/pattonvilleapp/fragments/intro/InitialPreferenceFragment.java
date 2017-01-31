7a8a7ef15d5308e145b0def8894be501faa6dfc5
Mitchell Skaggs <skaggsm333@gmail.com>
2017-01-31 16:26:18 -0600

    Moved inner classes into new package

---
+++ app/src/main/java/org/pattonvillecs/pattonvilleapp/fragments/intro/InitialPreferenceFragment.java
@@ -0,0 +1,24 @@
+package org.pattonvillecs.pattonvilleapp.fragments.intro;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.takisoft.fix.support.v7.preference.PreferenceFragmentCompat;
+
+import org.pattonvillecs.pattonvilleapp.R;
+
+/**
+ * Created by skaggsm on 1/31/17.
+ */
+public final class InitialPreferenceFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onCreatePreferencesFix(@Nullable Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.initial_preferences);
+    }
+}
