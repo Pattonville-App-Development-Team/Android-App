b2f713d68969581725a39f12cdd4d45f73cf3fc5
gadsonk <keturahj30@gmail.com>
2017-02-10 08:48:49 -0600

    Changed variable for factory name to match the directory csv file

--- app/src/main/java/org/pattonvillecs/pattonvilleapp/fragments/directory/DirectoryViewHolder.java
+++ app/src/main/java/org/pattonvillecs/pattonvilleapp/fragments/directory/DirectoryViewHolder.java
@@ -18,7 +18,7 @@ import eu.davidea.viewholders.FlexibleViewHolder;
 public class DirectoryViewHolder extends FlexibleViewHolder {
     private static final String TAG = "DirectoryViewHolder";
     final RecyclerView facultyView;
-    final TextView nameText, departmentText, extensionText;
+    final TextView nameText, longDesText, extensionText;
     final ImageButton emailButton;
 
     public DirectoryViewHolder(View view, FlexibleAdapter adapter) {
@@ -28,7 +28,7 @@ public class DirectoryViewHolder extends FlexibleViewHolder {
     public DirectoryViewHolder(View view, FlexibleAdapter adapter, boolean stickyHeader) {
         super(view, adapter, stickyHeader);
         nameText = (TextView) view.findViewById(R.id.directory_facultyName_textView);
-        departmentText = (TextView) view.findViewById(R.id.directory_facultyDepartment_textView);
+        longDesText = (TextView) view.findViewById(R.id.directory_facultyDepartment_textView);
         extensionText = (TextView) view.findViewById(R.id.directory_facultyExtension_textView);
         emailButton = (ImageButton) view.findViewById(R.id.directory_facultyEmail_imageButton);
         facultyView = (RecyclerView) view.findViewById(R.id.directory_detail_recyclerView);
