d9497c5ebbcd98bf95c6a16f5278335179364049
Nathaniel Skelton <nathan.hunter.skelton@gmail.com>
2017-02-27 09:44:13 -0600

    Cleaned NewsParsingUpdateData

--- app/src/main/java/org/pattonvillecs/pattonvilleapp/news/NewsParsingUpdateData.java
+++ app/src/main/java/org/pattonvillecs/pattonvilleapp/news/NewsParsingUpdateData.java
@@ -7,9 +7,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ConcurrentMap;
 
-/**
- * Created by Mitchell Skaggs on 2/13/17.
- */
 public class NewsParsingUpdateData {
     public static final int NEWS_LISTENER_ID = 412983751;
     private final ConcurrentMap<DataSource, List<NewsArticle>> newsData;
