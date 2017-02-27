0fb5f2a48eb9a2c7fc8e6943441fbdb96b3fab5b
Nathaniel Skelton <nathan.hunter.skelton@gmail.com>
2017-02-27 11:35:27 -0600

    Documented NewsDetailActivity

--- app/src/main/java/org/pattonvillecs/pattonvilleapp/news/NewsDetailActivity.java
+++ app/src/main/java/org/pattonvillecs/pattonvilleapp/news/NewsDetailActivity.java
@@ -24,9 +24,17 @@ import org.pattonvillecs.pattonvilleapp.PattonvilleApplication;
 import org.pattonvillecs.pattonvilleapp.R;
 import org.pattonvillecs.pattonvilleapp.news.articles.NewsArticle;
 
+/**
+ * Activity for displaying a news article's content
+ *
+ * @author Nathan Skelton
+ */
 public class NewsDetailActivity extends AppCompatActivity {
 
     private static final String TAG = NewsDetailActivity.class.getSimpleName();
+
+    private static final String CONTENTFORMATINGSTRING =
+            "<style>img{display: inline;height: auto;max-width: 100%;}</style>";
 
     private WebView mWebView;
     private SwipeRefreshLayout mRefreshLayout;
@@ -38,19 +46,26 @@ public class NewsDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_detail);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+
+        // Avoid null pointer warning with null check
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
         mRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.news_detail_refresh_layout);
+
+        // Defines the colors used for the refresh icon
         mRefreshLayout.setColorSchemeResources(R.color.colorPrimary, R.color.colorPrimaryDark);
         mRefreshLayout.setRefreshing(true);
 
         mWebView = (WebView) findViewById(R.id.news_detail_webview);
+
+        // Defines the background color to match the app's background
         mWebView.setBackgroundColor(Color.parseColor("#FAFAFA"));
         mWebView.setHorizontalScrollBarEnabled(false);
+
+        // Add listener to close the refresh layout when content loaded
         mWebView.setWebViewClient(new WebViewClient() {
 
             public void onPageFinished(WebView view, String url) {
@@ -63,34 +78,40 @@ public class NewsDetailActivity extends AppCompatActivity {
 
         setTitle("News");
 
+        // Set content for title and date
         ((TextView) findViewById(R.id.newsDetail_toolbar_title)).setText(newsArticle.getTitle());
         ((TextView) findViewById(R.id.newsDetail_toolbar_date)).setText(newsArticle.getFormattedDate());
 
         RequestQueue queue = PattonvilleApplication.get(this).getRequestQueue();
 
+        // Article content request using Volley
         StringRequest stringRequest = new StringRequest(Request.Method.GET, newsArticle.getPrivateUrl(),
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
 
-                        mWebView.loadData("<style>img{display: inline;height: auto;max-width: 100%;}</style>" +
+                        // When content found, load
+                        mWebView.loadData(CONTENTFORMATINGSTRING +
                                 NewsArticle.formatContent(response), "text/html", null);
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
 
+                // When unable to get content, inform the user
                 Toast.makeText(getApplicationContext(), "Unable to load content", Toast.LENGTH_SHORT).show();
                 mRefreshLayout.setRefreshing(false);
                 mRefreshLayout.setEnabled(false);
             }
         });
 
+        // If the cache has the link's content save, parse and display
+        // Otherwise, pull the data
         if (queue.getCache().get(stringRequest.getCacheKey()) != null
                 && queue.getCache().get(stringRequest.getCacheKey()).data != null) {
             Log.e(TAG, "Using Cache");
-            mWebView.loadData("<style>img{display: inline;height: auto;max-width: 100%;}</style>" +
-                    NewsArticle.formatContent(new String(queue.getCache().get(stringRequest.getCacheKey()).data)), "text/html", null);
+            mWebView.loadData(CONTENTFORMATINGSTRING + NewsArticle.formatContent(
+                    new String(queue.getCache().get(stringRequest.getCacheKey()).data)), "text/html", null);
         } else {
             queue.add(stringRequest);
         }
@@ -109,6 +130,8 @@ public class NewsDetailActivity extends AppCompatActivity {
         switch (item.getItemId()) {
 
             case R.id.newsDetail_share:
+
+                // Creation of a Share Intent, as shown by android developer guides
                 Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
                 sharingIntent.setType("text/plain");
                 sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, newsArticle.getPublicUrl());
@@ -116,10 +139,11 @@ public class NewsDetailActivity extends AppCompatActivity {
                 break;
 
             case android.R.id.home:
+
+                // If back pressed, finish activity
                 finish();
                 break;
         }
-
         return true;
     }
 }
