876d21889051f2a9bb3bc01496c3c2f6db5bd7d9
Mitchell Skaggs <skaggsm333@gmail.com>
2017-02-28 11:00:14 -0600

    Made pinned button insert the pinned event into the database and all bound items react to database updates. Events now instantly pin all other events with the same UID.

--- app/src/main/java/org/pattonvillecs/pattonvilleapp/fragments/calendar/events/EventFlexibleItem.java
+++ app/src/main/java/org/pattonvillecs/pattonvilleapp/fragments/calendar/events/EventFlexibleItem.java
@@ -2,12 +2,19 @@ package org.pattonvillecs.pattonvilleapp.fragments.calendar.events;
 
 import android.app.Activity;
 import android.app.ActivityOptions;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.Intent;
+import android.database.ContentObserver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.util.Log;
 import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +23,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.varunest.sparkbutton.SparkButton;
+import com.varunest.sparkbutton.SparkEventListener;
 
 import net.fortuna.ical4j.model.component.VEvent;
 import net.fortuna.ical4j.model.property.Location;
@@ -24,6 +33,7 @@ import net.fortuna.ical4j.model.property.Summary;
 import org.pattonvillecs.pattonvilleapp.DataSource;
 import org.pattonvillecs.pattonvilleapp.R;
 import org.pattonvillecs.pattonvilleapp.fragments.calendar.CalendarEventDetailsActivity;
+import org.pattonvillecs.pattonvilleapp.fragments.calendar.pinned.PinnedEventsContract;
 
 import java.util.List;
 
@@ -46,6 +56,7 @@ public class EventFlexibleItem extends AbstractSectionableItem<EventFlexibleItem
             return new EventFlexibleItem[size];
         }
     };
+    private static final String TAG = EventFlexibleItem.class.getSimpleName();
 
     public final DataSource dataSource;
     public final VEvent vEvent;
@@ -59,6 +70,60 @@ public class EventFlexibleItem extends AbstractSectionableItem<EventFlexibleItem
         super(null);
         this.dataSource = dataSource;
         this.vEvent = vEvent;
+    }
+
+    private static Activity getActivity(View v) {
+        Context context = v.getContext();
+        while (context instanceof ContextWrapper) {
+            if (context instanceof Activity) {
+                return (Activity) context;
+            }
+            context = ((ContextWrapper) context).getBaseContext();
+        }
+        return null;
+    }
+
+    private static String cursorToString(Cursor cursor) {
+        StringBuilder cursorString = new StringBuilder();
+        if (cursor.moveToFirst()) {
+            String[] columnNames = cursor.getColumnNames();
+            for (String name : columnNames)
+                cursorString.append(String.format("%s ][ ", name));
+            cursorString.append('\n');
+            do {
+                for (String name : columnNames) {
+                    cursorString.append(String.format("%s ][ ", cursor.getString(cursor.getColumnIndex(name))));
+                }
+                cursorString.append('\n');
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        return cursorString.toString();
+    }
+
+    private static void deleteEntriesWithUid(Context context, String uid) {
+        context.getContentResolver().delete(
+                PinnedEventsContract.PinnedEventsTable.CONTENT_URI,
+                PinnedEventsContract.PinnedEventsTable.COLUMN_NAME_UID + "=?",
+                new String[]{uid});
+    }
+
+
+    private static void insertEntry(Context context, String uid) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(PinnedEventsContract.PinnedEventsTable.COLUMN_NAME_UID, uid);
+        context.getContentResolver().insert(
+                PinnedEventsContract.PinnedEventsTable.CONTENT_URI,
+                contentValues);
+    }
+
+    private static Cursor requeryByUid(Context context, String uid) {
+        return context.getContentResolver().query(
+                PinnedEventsContract.PinnedEventsTable.CONTENT_URI,
+                null,
+                PinnedEventsContract.PinnedEventsTable.COLUMN_NAME_UID + "=?",
+                new String[]{uid},
+                null);
     }
 
     @Override
@@ -91,9 +156,15 @@ public class EventFlexibleItem extends AbstractSectionableItem<EventFlexibleItem
     }
 
     @Override
-    public void bindViewHolder(FlexibleAdapter adapter, EventViewHolder holder, final int position, List payloads) {
+    public void bindViewHolder(FlexibleAdapter adapter, final EventViewHolder holder, final int position, List payloads) {
+        if (holder.cursor != null && !holder.cursor.isClosed()) {
+            holder.cursor.close();
+            holder.cursor = null;
+            //Make is so.
+        }
         Summary summary = vEvent.getSummary();
         Location location = vEvent.getLocation();
+        final String uid = vEvent.getUid().getValue();
 
         if (summary != null)
             holder.topText.setText(summary.getValue());
@@ -133,18 +204,42 @@ public class EventFlexibleItem extends AbstractSectionableItem<EventFlexibleItem
                     }
                 }
             }
+        });
+        holder.sparkButton.setEventListener(new SparkEventListener() {
+            @Override
+            public void onEvent(ImageView button, boolean buttonState) {
+                Log.i(TAG, "State: " + buttonState + " UID: " + uid);
+                Log.v(TAG, "Full table is now: \n" + cursorToString(holder.view.getContext().getContentResolver().query(PinnedEventsContract.PinnedEventsTable.CONTENT_URI, null, null, null, null)));
 
-            private Activity getActivity(View v) {
-                Context context = v.getContext();
-                while (context instanceof ContextWrapper) {
-                    if (context instanceof Activity) {
-                        return (Activity) context;
-                    }
-                    context = ((ContextWrapper) context).getBaseContext();
+                if (buttonState) {
+                    deleteEntriesWithUid(button.getContext(), uid);
+                    insertEntry(button.getContext(), uid);
+                } else {
+                    deleteEntriesWithUid(button.getContext(), uid);
                 }
-                return null;
             }
         });
+        ContentObserver contentObserver = new ContentObserver(new Handler(Looper.getMainLooper())) {
+            @Override
+            public void onChange(boolean selfChange) {
+                onChange(selfChange, null);
+            }
+
+            @Override
+            public void onChange(boolean selfChange, Uri uri) {
+                if (holder.cursor != null) // Only needed for the first check
+                    holder.cursor.close(); // The old view is closed, not needed anymore
+                holder.cursor = requeryByUid(holder.view.getContext(), uid); // Get a new look at the dataset
+                holder.cursor.registerContentObserver(this); // Attach this anonymous class to it so we keep getting updates
+
+                Log.v(TAG, "Count of " + uid + ": " + holder.cursor.getCount());
+                if (holder.cursor.getCount() > 0)
+                    holder.sparkButton.setChecked(true);
+                else
+                    holder.sparkButton.setChecked(false);
+            }
+        };
+        contentObserver.onChange(false); // Fire the first "event" to kick-start the requerying process. After this, the ContentObserver is automatically reregistered and cursors are closed/created
     }
 
     @Override
@@ -167,9 +262,11 @@ public class EventFlexibleItem extends AbstractSectionableItem<EventFlexibleItem
      * Created by Mitchell Skaggs on 1/4/17.
      */
     public static class EventViewHolder extends FlexibleViewHolder {
-        final TextView topText, bottomText, shortSchoolName;
-        final ImageView schoolColorImageView;
-        final View view;
+        private final TextView topText, bottomText, shortSchoolName;
+        private final ImageView schoolColorImageView;
+        private final View view;
+        private final SparkButton sparkButton;
+        private Cursor cursor;
 
         public EventViewHolder(View view, FlexibleAdapter adapter) {
             this(view, adapter, false);
@@ -182,6 +279,7 @@ public class EventFlexibleItem extends AbstractSectionableItem<EventFlexibleItem
             bottomText = (TextView) view.findViewById(R.id.text_bottom);
             schoolColorImageView = (ImageView) view.findViewById(R.id.school_color_circle);
             shortSchoolName = (TextView) view.findViewById(R.id.school_short_name);
+            sparkButton = (SparkButton) view.findViewById(R.id.pinned_button);
         }
     }
 }
