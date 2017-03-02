da4a5f717968a2dfb0cafc13a22c23d0768ebe21
gadsonk <keturahj30@gmail.com>
2017-03-02 08:03:43 -0600

    Added setters for the directory key and faculty rank.

--- app/src/main/java/org/pattonvillecs/pattonvilleapp/Faculty.java
+++ app/src/main/java/org/pattonvillecs/pattonvilleapp/Faculty.java
@@ -9,6 +9,7 @@ public class Faculty {
     private String mFirstName, mLastName, mLongDesc, mLocation, mEmail, mOffice1,
             mExtension1, mOffice2, mExtension2, mOffice3, mExtension3;
     private int mRank;
+    private DataSource directoryKey;
 
     //list of phone numbers and extension, location
 
@@ -32,7 +33,8 @@ public class Faculty {
         this.mExtension2 = mExtension2;
         this.mOffice3 = mOffice3;
         this.mExtension3 = mExtension3;
-        mRank = getRank();
+        setRank();
+        setDirectoryKey(mLocation);
     }
 
     public String getFirstName() {
@@ -79,8 +81,75 @@ public class Faculty {
         return mExtension3;
     }
 
-    private int getRank() {
-        return 0;
+    public int getRank() {
+        return mRank;
+    }
+
+    public DataSource getDirectoryKey() {
+        return directoryKey;
+    }
+
+    private void setDirectoryKey(String location) {
+        DataSource key = null;
+        switch (location.trim().toUpperCase()) {
+            case "BRIDGEWAY ELEMENTARY":
+                key = DataSource.BRIDGEWAY_ELEMENTARY;
+                break;
+            case "ROBERT DRUMMOND ELEMENTARY":
+                key = DataSource.DRUMMOND_ELEMENTARY;
+                break;
+            case "HOLMAN MIDDLE SCHOOL":
+                key = DataSource.HOLMAN_MIDDLE_SCHOOL;
+                break;
+            case "PATTONVILLE HEIGHTS":
+                key = DataSource.HEIGHTS_MIDDLE_SCHOOL;
+                break;
+            case "PARKWOOD ELEMENTARY":
+                key = DataSource.PARKWOOD_ELEMENTARY;
+                break;
+            case "ROSE ACRES ELEMENTARY":
+                key = DataSource.ROSE_ACRES_ELEMENTARY;
+                break;
+            case "REMINGTON TRADITIONAL":
+                key = DataSource.REMINGTON_TRADITIONAL_SCHOOL;
+                break;
+            case "PATTONVILLE HIGH SCHOOL":
+            case "POSITIVE SCHOOL":
+                key = DataSource.HIGH_SCHOOL;
+                break;
+            case "WILLOW BROOK ELEMENTARY":
+                key = DataSource.WILLOW_BROOK_ELEMENTARY;
+                break;
+            case "LEARNING CENTER":
+            default:
+                key = DataSource.DISTRICT;
+        }
+        directoryKey = key;
+    }
+
+    public void setRank() {
+        if (mLongDesc.contains("PRINCIPAL")) {
+            if (mLongDesc.contains("ASSOCIATE PRINCIPAL")) {
+                mRank = 1;
+            }
+            if (mLongDesc.contains("ASSITANT PRINCIPAL")) {
+                mRank = 2;
+            }
+            mRank = 0;
+        } else if (org.apache.commons.lang3.StringUtils.containsIgnoreCase(mLongDesc, "TEA") ||
+                mLongDesc.contains("TCHR")) {
+            mRank = 3;
+        } else if (mLongDesc.contains("SECRETARY")) {
+            if (mLongDesc.contains("EXECUTIVE SECRETARY 1")) {
+                mRank = 4;
+            }
+            if (mLongDesc.contains("EXECUTIVE SECRETARY 2")) {
+                mRank = 5;
+            }
+            mRank = 6;
+        } else {
+            mRank = 7;
+        }
     }
 }
 
