8745e612e64d89856e78f031404d78c7be5e4d4d
Nathaniel Skelton <nathan.hunter.skelton@gmail.com>
2017-04-21 09:39:25 -0500

    Cleaned up code with lambda, now documented

--- app/src/main/java/org/pattonvillecs/pattonvilleapp/preferences/SettingsActivity.java
+++ app/src/main/java/org/pattonvillecs/pattonvilleapp/preferences/SettingsActivity.java
@@ -1,13 +1,18 @@
 package org.pattonvillecs.pattonvilleapp.preferences;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import org.pattonvillecs.pattonvilleapp.R;
 
+/**
+ * Activity plus inner fragment to handle preferences for the application
+ *
+ * @author Nathan Skelton
+ */
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
@@ -27,6 +32,9 @@ public class SettingsActivity extends AppCompatActivity {
         Log.d("SELECTED SCHOOLS", "These are selected: " + PreferenceUtils.getSelectedSchoolsSet(this));
     }
 
+    /**
+     * Our preference fragment which is inserted into SettingsActivity
+     */
     public static class SettingsFragment extends PreferenceFragment {
 
         private NumberPickerPreference mNewsPreference;
@@ -38,34 +46,39 @@ public class SettingsActivity extends AppCompatActivity {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.main_preferences);
 
+            Context context = getActivity().getApplicationContext();
+
+            // Setup the summary for each preference, through setting an onPreferenceChangeListener
+            // and setting the initial summary string
+
             mNewsPreference = (NumberPickerPreference) findPreference(getString(R.string.key_home_newsamount));
-            mNewsPreference.setSummary(PreferenceUtils.getSharedPreferences(getActivity().getApplicationContext()).getInt(getString(R.string.key_home_newsamount), 3) + " News Articles");
-            mNewsPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-                @Override
-                public boolean onPreferenceChange(Preference preference, Object newValue) {
-                    mNewsPreference.setSummary(newValue + " News Articles");
-                    return true;
-                }
+            mNewsPreference.setSummary(PreferenceUtils.getSharedPreferences(context)
+                    .getInt(getString(R.string.key_home_newsamount), 3) +
+                    context.getString(R.string.preference_summary_news));
+            mNewsPreference.setOnPreferenceChangeListener((preference, newValue) -> {
+                mNewsPreference.setSummary(newValue +
+                        context.getString(R.string.preference_summary_news));
+                return true;
             });
 
             mUpcomingEventsPreference = (NumberPickerPreference) findPreference(getString(R.string.key_home_eventsamount));
-            mUpcomingEventsPreference.setSummary(PreferenceUtils.getSharedPreferences(getActivity().getApplicationContext()).getInt(getString(R.string.key_home_eventsamount), 3) + " Upcoming Events");
-            mUpcomingEventsPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-                @Override
-                public boolean onPreferenceChange(Preference preference, Object newValue) {
-                    mUpcomingEventsPreference.setSummary(newValue + " Upcoming Events");
-                    return true;
-                }
+            mUpcomingEventsPreference.setSummary(PreferenceUtils.getSharedPreferences(context)
+                    .getInt(getString(R.string.key_home_eventsamount), 3) +
+                    context.getString(R.string.preference_summary_upcoming));
+            mUpcomingEventsPreference.setOnPreferenceChangeListener((preference, newValue) -> {
+                mUpcomingEventsPreference.setSummary(newValue +
+                        context.getString(R.string.preference_summary_upcoming));
+                return true;
             });
 
             mPinnedEventsPreference = (NumberPickerPreference) findPreference(getString(R.string.key_home_pinnedamount));
-            mPinnedEventsPreference.setSummary(PreferenceUtils.getSharedPreferences(getActivity().getApplicationContext()).getInt(getString(R.string.key_home_pinnedamount), 3) + " Pinned Events");
-            mPinnedEventsPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-                @Override
-                public boolean onPreferenceChange(Preference preference, Object newValue) {
-                    mPinnedEventsPreference.setSummary(newValue + " Pinned Events");
-                    return true;
-                }
+            mPinnedEventsPreference.setSummary(PreferenceUtils.getSharedPreferences(context)
+                    .getInt(getString(R.string.key_home_pinnedamount), 3) +
+                    context.getString(R.string.preference_summary_pinned));
+            mPinnedEventsPreference.setOnPreferenceChangeListener((preference, newValue) -> {
+                mPinnedEventsPreference.setSummary(newValue +
+                        context.getString(R.string.preference_summary_pinned));
+                return true;
             });
         }
     }
