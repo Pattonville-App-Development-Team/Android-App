80c98533ba81ad91d1b9ec73dd094168f7c53a53
Mitchell Skaggs <skaggsm333@gmail.com>
2017-11-22 20:45:41 -0600

    Adjusted event formatting and moved the formatter to a companion object

--- app/src/main/java/org/pattonvillecs/pattonvilleapp/ui/calendar/month/PinnableCalendarEventItem.kt
+++ app/src/main/java/org/pattonvillecs/pattonvilleapp/ui/calendar/month/PinnableCalendarEventItem.kt
@@ -9,9 +9,11 @@ import eu.davidea.flexibleadapter.items.IFlexible
 import eu.davidea.flexibleadapter.items.IHolder
 import eu.davidea.viewholders.FlexibleViewHolder
 import org.jetbrains.anko.coroutines.experimental.bg
+import org.pattonvillecs.pattonvilleapp.DataSource
 import org.pattonvillecs.pattonvilleapp.R
 import org.pattonvillecs.pattonvilleapp.calendar.events.EventFlexibleItem.getDataSourcesSpannableStringBuilder
 import org.pattonvillecs.pattonvilleapp.model.calendar.PinnableCalendarEvent
+import org.pattonvillecs.pattonvilleapp.ui.calendar.month.PinnableCalendarEventItem.PinnableCalendarEventItemViewHolder
 import org.threeten.bp.ZoneId
 import org.threeten.bp.format.DateTimeFormatter
 import org.threeten.bp.format.FormatStyle
@@ -29,15 +31,13 @@ data class PinnableCalendarEventItem(private val pinnableCalendarEvent: Pinnable
     override fun getLayoutRes(): Int = R.layout.calendar_dateless_event_list_item
 
     override fun bindViewHolder(adapter: FlexibleAdapter<out IFlexible<*>>, holder: PinnableCalendarEventItemViewHolder, position: Int, payloads: MutableList<Any?>?) {
-        val formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)
-                .withLocale(Locale.getDefault())
-                .withZone(ZoneId.systemDefault())
+
 
         holder.topText.text = pinnableCalendarEvent.calendarEvent.summary
 
-        holder.bottomText.text = formatter.format(pinnableCalendarEvent.calendarEvent.startDate)
+        holder.bottomText.text = FORMATTER.format(pinnableCalendarEvent.calendarEvent.startDate)
 
-        holder.shortSchoolName.text = getDataSourcesSpannableStringBuilder(pinnableCalendarEvent.dataSources, adapter.recyclerView.context)
+        holder.shortSchoolName.text = getDataSourcesSpannableStringBuilder(pinnableCalendarEvent.dataSources.sortedWith(DataSource.DEFAULT_ORDERING), adapter.recyclerView.context)
 
         holder.sparkButton.isChecked = pinnableCalendarEvent.pinned
         holder.sparkButton.setEventListener({ _, buttonState ->
@@ -49,12 +49,18 @@ data class PinnableCalendarEventItem(private val pinnableCalendarEvent: Pinnable
             }
         })
     }
-}
 
-class PinnableCalendarEventItemViewHolder(view: View, adapter: FlexibleAdapter<out IFlexible<*>>, stickyHeader: Boolean = false) : FlexibleViewHolder(view, adapter, stickyHeader) {
-    val topText = view.findViewById<TextView>(R.id.text_top)!!
-    val bottomText = view.findViewById<TextView>(R.id.text_bottom)!!
-    val shortSchoolName = view.findViewById<TextView>(R.id.school_short_names)!!
-    val sparkButton = view.findViewById<SparkButton>(R.id.pinned_button)!!
-    val adapter = adapter as CalendarEventFlexibleAdapter
+    companion object {
+        val FORMATTER: DateTimeFormatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG, FormatStyle.SHORT)
+                .withLocale(Locale.getDefault())
+                .withZone(ZoneId.systemDefault())
+    }
+
+    class PinnableCalendarEventItemViewHolder(view: View, adapter: FlexibleAdapter<out IFlexible<*>>, stickyHeader: Boolean = false) : FlexibleViewHolder(view, adapter, stickyHeader) {
+        val topText = view.findViewById<TextView>(R.id.text_top)!!
+        val bottomText = view.findViewById<TextView>(R.id.text_bottom)!!
+        val shortSchoolName = view.findViewById<TextView>(R.id.school_short_names)!!
+        val sparkButton = view.findViewById<SparkButton>(R.id.pinned_button)!!
+        val adapter = adapter as CalendarEventFlexibleAdapter
+    }
 }
