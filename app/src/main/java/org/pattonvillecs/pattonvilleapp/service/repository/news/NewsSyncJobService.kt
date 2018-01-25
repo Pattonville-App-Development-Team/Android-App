12a05fc89e68db52363bb4ffba8f6de122ad8758
Mitchell Skaggs <skaggsm333@gmail.com>
2018-01-25 16:03:10 -0600

    Updated copyright notice on all files

--- app/src/main/java/org/pattonvillecs/pattonvilleapp/service/repository/news/NewsSyncJobService.kt
+++ app/src/main/java/org/pattonvillecs/pattonvilleapp/service/repository/news/NewsSyncJobService.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Mitchell Skaggs, Keturah Gadson, Ethan Holtgrieve, Nathan Skelton, Pattonville School District
+ * Copyright (C) 2017 - 2018 Mitchell Skaggs, Keturah Gadson, Ethan Holtgrieve, Nathan Skelton, Pattonville School District
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
